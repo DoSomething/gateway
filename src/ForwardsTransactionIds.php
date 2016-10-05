d2c1a07fcc94be071b32c622f8c6cce85b5fbd2e
Chloe Lee <chloealessandra@gmail.com>
2016-10-05 12:33:35 -0400

    builds out runForwardsTransactionIdsTasks

--- src/ForwardsTransactionIds.php
+++ src/ForwardsTransactionIds.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace DoSomething\Gateway;
+use Request;
 
 trait ForwardsTransactionIds
 {
@@ -11,6 +12,14 @@ trait ForwardsTransactionIds
      */
     function runForwardsTransactionIdsTasks($method, &$path, &$options, &$withAuthorization)
     {
-        // ...
+
+        // Get transaction ID and append microtime to end of Transaction ID.
+        // TODO: prepend application name that is making the request to Transaction ID.
+        $newTransactionIDHeader = ['X-Request-ID' => Request::header('X-Request-ID') . '-' . microtime(TRUE)];
+
+        // add to header
+        $options['headers'] = array_merge($this->defaultHeaders, $authorizationHeader, $newTransactionIDHeader);
+
+        // add to laravel logs (doesn't this need to happen in the raw function though since we want to log that it has been sent?) - no this happens at the end of the handle function of SendReportbackToPhoenix.
     }
 }
