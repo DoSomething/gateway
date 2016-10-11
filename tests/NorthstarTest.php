e727da3e2797bdef938fa46ef19efea64f9840c1
David Furnes <david@dfurnes.com>
2016-10-11 11:46:35 -0400

    Add a simple instantiation & authorized request test.

--- tests/NorthstarTest.php
+++ tests/NorthstarTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use GuzzleHttp\Psr7\Response;
+
 class AuthorizesWithNorthstarTest extends TestCase
 {
     protected $defaultConfig = [
@@ -21,6 +23,35 @@ class AuthorizesWithNorthstarTest extends TestCase
      */
     public function testNorthstarApiClient()
     {
-        $this->markTestIncomplete();
+        $restClient = new MockNorthstar($this->defaultConfig, [/* ... */]);
+
+        $this->assertEquals((string) $restClient->getBaseUri(), 'https://northstar-phpunit.dosomething.org');
+    }
+
+    /**
+     * Test that we can request an access token and use it to retrieve
+     * a page using client credentials grant.
+     */
+    public function testGetAuthorizedPage()
+    {
+        $restClient = new MockNorthstar($this->defaultConfig, [
+            new Response(200, ['Content-Type' => 'application/json'], json_encode([
+                'token_type' => 'Bearer',
+                'access_token' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImUwNmJjMDhlZGE5NjRiZmQwNjYzZjc5MzgxNDhkZ
+                TYzYjYzNzkxYmMxOTFhNTEwYmY4MDZlYzJhMGZiNjUzYzc1MjFhOWIzYTFmM2NjNjcyIn0.eyJpc3MiOiJodHRwOlwvXC9ub3J0aHN0Y
+                XIuZGV2OjgwMDAiLCJhdWQiOiJ0cnVzdGVkLXRlc3QtY2xpZW50IiwianRpIjoiZTA2YmMwOGVkYTk2NGJmZDA2NjNmNzkzODE0OGRlN
+                jNiNjM3OTFiYzE5MWE1MTBiZjgwNmVjMmEwZmI2NTNjNzUyMWE5YjNhMWYzY2M2NzIiLCJpYXQiOjE0NzYyMDEzMzEsIm5iZiI6MTQ3N
+                jIwMTMzMSwiZXhwIjoxNDc2MjA0OTMxLCJzdWIiOiIiLCJyb2xlIjoiIiwic2NvcGVzIjpbXX0.aFyfvihTvumdWPgtfXxKQZYKVGJw-
+                DnvUZO-XXNUgKYy8ngq0OTHMV00_VEqJVHPZVZ1FQv7sq-LcxpSpEM-VgfPsNUVuhJ8E3nX63ntTQQiv_CFBvippb1LJqVhfE7gzaKQc
+                eatw5dT25nDlzRQIrUY1kz1exGHUozvkHAeJ_g',
+                'expires_in' => 3600,
+            ])),
+            new Response(200, ['Content-Type' => 'application/json'], json_encode([
+                'status' => 'good',
+            ])),
+        ]);
+
+        $response = $restClient->get('status');
+        $this->assertEquals($response, ['status' => 'good']);
     }
 }
