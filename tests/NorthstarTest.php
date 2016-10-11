6b1a7f823f60a6682aae72fac01e7a6ef78af2c9
David Furnes <david@dfurnes.com>
2016-10-11 17:32:51 -0400

    Add a test for the user index.

--- tests/NorthstarTest.php
+++ tests/NorthstarTest.php
@@ -54,4 +54,60 @@ class AuthorizesWithNorthstarTest extends TestCase
         $response = $restClient->get('status');
         $this->assertEquals($response, ['status' => 'good']);
     }
+
+    /**
+     * Test that we can use the "all users" endpoint.
+     */
+    public function testGetAllUsers()
+    {
+        $restClient = new MockNorthstar($this->defaultConfig, [
+            new Response(200, ['Content-Type' => 'application/json'], json_encode([
+                'token_type' => 'Bearer',
+                'access_token' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImUwNmJjMDhlZGE5NjRiZmQwNjYzZjc5MzgxNDhkZ
+                TYzYjYzNzkxYmMxOTFhNTEwYmY4MDZlYzJhMGZiNjUzYzc1MjFhOWIzYTFmM2NjNjcyIn0.eyJpc3MiOiJodHRwOlwvXC9ub3J0aHN0Y
+                XIuZGV2OjgwMDAiLCJhdWQiOiJ0cnVzdGVkLXRlc3QtY2xpZW50IiwianRpIjoiZTA2YmMwOGVkYTk2NGJmZDA2NjNmNzkzODE0OGRlN
+                jNiNjM3OTFiYzE5MWE1MTBiZjgwNmVjMmEwZmI2NTNjNzUyMWE5YjNhMWYzY2M2NzIiLCJpYXQiOjE0NzYyMDEzMzEsIm5iZiI6MTQ3N
+                jIwMTMzMSwiZXhwIjoxNDc2MjA0OTMxLCJzdWIiOiIiLCJyb2xlIjoiIiwic2NvcGVzIjpbXX0.aFyfvihTvumdWPgtfXxKQZYKVGJw-
+                DnvUZO-XXNUgKYy8ngq0OTHMV00_VEqJVHPZVZ1FQv7sq-LcxpSpEM-VgfPsNUVuhJ8E3nX63ntTQQiv_CFBvippb1LJqVhfE7gzaKQc
+                eatw5dT25nDlzRQIrUY1kz1exGHUozvkHAeJ_g',
+                'expires_in' => 3600,
+            ])),
+            new Response(200, ['Content-Type' => 'application/json'], json_encode([
+                'data' => [
+                    [
+                        'id' => '5480c950bffebc651c8b456f',
+                        'first_name' => 'John',
+                        'last_name' => 'Doe',
+                    ],
+                    [
+                        'id' => '5480c950ce5fbc2145eb7721',
+                        'first_name' => 'Jane',
+                        'last_name' => 'Smith',
+                    ],
+                ],
+                'meta' => [
+                    'pagination' => [
+                        'total' => 65,
+                        'count' => 20,
+                        'per_page' => 15,
+                        'current_page' => 1,
+                        'total_pages' => 5,
+                        'links' => [
+                            'next' => 'https://northstar.dosomething.org/v1/users?page=2',
+                        ],
+                    ],
+                ],
+            ])),
+        ]);
+
+        $response = $restClient->getAllUsers();
+
+        // It should successfully serialize into a collection.
+        $this->assertInstanceOf(\DoSomething\Gateway\Common\ApiCollection::class, $response);
+
+        // And we should be able to traverse and read values from that.
+        $this->assertEquals('John', $response[0]->first_name);
+        $this->assertEquals('Jane', $response[1]->first_name);
+        $this->assertEquals(2, $response->count());
+    }
 }
