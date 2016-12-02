d15c5d360b2e4a85e3ceb017abc89dae2d9fd65f
David Furnes <david@dfurnes.com>
2016-12-02 14:40:35 -0500

    Indexes must be dropped by _index_ name, not column name!

--- src/Laravel/Migrations/2016_06_29_205018_add_oauth_fields_to_users.php
+++ src/Laravel/Migrations/2016_06_29_205018_add_oauth_fields_to_users.php
@@ -33,7 +33,7 @@ class AddOauthFieldsToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropIndex('northstar_id');
+            $table->dropIndex('users_northstar_id_unique');
             $table->dropColumn('northstar_id');
 
             $table->dropColumn('access_token');
