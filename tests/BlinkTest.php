4e2b9bc6de35d52dab2b9a3ed095dd484e32d454
Sergii Tkachenko <hi@sergii.org>
2017-08-10 14:58:28 -0400

    Create Blink's testUserCreate()

--- tests/BlinkTest.php
+++ tests/BlinkTest.php
@@ -27,9 +27,9 @@ class BlinkTest extends PHPUnit_Framework_TestCase
     ];
 
     /**
-     * Test that we can notify blink o user creation.
+     * Test that we can notify blink of user creation.
      */
-    public function testCreateSignup()
+    public function testUserCreate()
     {
         $blinkRestClient = new MockBlink($this->authorizedConfig, [
             new BlinkResponse,
@@ -42,6 +42,33 @@ class BlinkTest extends PHPUnit_Framework_TestCase
         $northstarUser = $northstarRestClient->getUser('email', 'kitty@xavierinstitute.edu');
 
         $result = $blinkRestClient->userCreate($northstarUser->toArray());
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Test that we can notify blink of user campaign signup.
+     */
+    public function testUserSigup()
+    {
+        // Input data.
+        $rogueSignupPayload = [
+            'id' => 4036838,
+            'northstar_id' => '598ca42c10707d7680749f81',
+            'campaign_id' => 7,
+            'campaign_run_id' => 7818,
+            'quantity' => 12,
+            'quantity_pending' => null,
+            'why_participated' => 'I love to test!',
+            'source' => 'niche',
+            'created_at' => '2017-08-10 18:21:35',
+            'updated_at' => '2017-08-10 18:21:35',
+        ];
+
+        $blinkRestClient = new MockBlink($this->authorizedConfig, [
+            new BlinkResponse,
+        ]);
+
+        $result = $blinkRestClient->userSignup($rogueSignupPayload);
         $this->assertTrue($result);
     }
 }
