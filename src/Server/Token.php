2b7a128fb3667a08c09d304a259dff683a9c78ea
David Furnes <david@dfurnes.com>
2019-09-11 14:51:35 -0400

    Why you fail?

--- src/Server/Token.php
+++ src/Server/Token.php
@@ -166,6 +166,7 @@ class Token
             // We've made it! Save the details on the validator.
             return $token;
         } catch (\InvalidArgumentException $exception) {
+            dump($request->bearerToken(), $exception, $exception->getMessage());
             throw new AccessDeniedException('Could not parse JWT.', $exception->getMessage());
         } catch (\RuntimeException $exception) {
             throw new AccessDeniedException('Error while decoding JWT contents.');
